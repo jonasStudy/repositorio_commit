0352482434c14bdcb927c384d21fcc5cfa6dccc1
jonasStudy <jonasborgessribeiro@gmail.com>
2023-01-01 15:48:31 -0300

    Adicionando mudancas na primeira funcionalidade

--- Funcionalidade.java
+++ Funcionalidade.java
@@ -6,5 +6,14 @@ public class Funcionalidade {
         int valor = 2023 - 1994;
         System.out.println("Minha idade atual é: " + valor);
 
+        int quantidadePessoas = 1;
+        int idade = 12;
+
+        if (quantidadePessoas >= 2 || idade >= 18) {
+            System.out.println("Pode entrar!");
+        } else {
+            System.out.println("Entrada não permitida!");
+        }
+
     }
 }
