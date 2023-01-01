7e051dc1692a3b632f57d30e7e7fefa7bc6eea79
jonasStudy <jonasborgessribeiro@gmail.com>
2023-01-01 15:30:01 -0300

    Adicionando a primeira Funcionalidade

---
+++ Funcionalidade.java
@@ -0,0 +1,10 @@
+
+public class Funcionalidade {
+    public static void main(String[] args) {
+        System.out.println("Adicionando arquivo para teste");
+
+        int valor = 2023 - 1994;
+        System.out.println("Minha idade atual é: " + valor);
+
+    }
+}
